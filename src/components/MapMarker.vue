9c09ec3c04d57488856eeec3f739341b54137aa2
daniel cheang <daniel@cpttm.org.mo>
2024-05-02 12:04:36 +0800

    add google map

---
+++ src/components/MapMarker.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { GoogleMap, Marker } from 'vue3-google-map'
+const center = { lat: 40.689247, lng: -74.044502 }
+</script>
+
+<template>
+  <div>
+    <GoogleMap
+      api-key="---"
+      style="width: 100%; height: 500px"
+      :center="center"
+      :zoom="15"
+      >
+      <Marker :options="{ position: center }" />
+    </GoogleMap>
+  </div>
+</template>
+
+<style scoped>
+</style>
